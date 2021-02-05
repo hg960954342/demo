f786a14364aa3762184aa475640d9addc3c90bf7
钢 胡 <BIGBIGGANG@live.com>
2021-02-05 10:36:33 +0800

    2022

---
+++ src/main/java/com/example/demo/Test.java
@@ -0,0 +1,48 @@
+package com.example.demo;
+
+import lombok.extern.slf4j.Slf4j;
+import lombok.val;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.List;
+
+@Component
+@Slf4j
+public class Test {
+    @PostConstruct
+    @Scheduled(cron = "0/2 * * * * ?")
+    public void test(){
+        double x = 20001.2f;
+        double y = 13000f;
+        double z = x-y;
+
+
+
+        HashSet<String> sites = new HashSet<String>();
+        sites.add("Google");
+        sites.add("Runoob");
+        sites.add("Taobao");
+        sites.add("Zhihu");
+        sites.add("Runoob");  // 重复的元素不会被添加
+        System.out.print(date()+"   "+sites+"\n"+"\n");
+    }
+
+    public String date(){
+        SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        return String.valueOf(df.format(new Date()));
+    }
+
+
+}
+
+
+
+
+
